6dd2d39684a22fd7f83fd9c966045c7d0880772a
shakeel5552005 <shakeel5552005@gmail.com>
2026-07-10 22:54:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
 import streamlit as st
+import requests
 
-st.title("My First Streamlit App")
+API_KEY = "YOUR_API_KEY"
 
-st.write("Welcome Shakeel!")
+st.set_page_config(page_title="Weather App", page_icon="🌦️")
 
-name = st.text_input("Enter your name")
+st.title("🌦️ Weather App")
 
-if name:
-    st.success(f"Hello {name}!")
-    
+city = st.text_input("Enter City Name")
+
+if st.button("Get Weather"):
+
+    if city == "":
+        st.warning("Please enter a city name.")
+
+    else:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+        response = requests.get(url)
+
+        data = response.json()
+
+        if response.status_code == 200:
+
+            st.success(f"Weather in {city}")
+
+            st.write(f"🌡 Temperature : {data['main']['temp']} °C")
+
+            st.write(f"🤗 Feels Like : {data['main']['feels_like']} °C")
+
+            st.write(f"💧 Humidity : {data['main']['humidity']} %")
+
+            st.write(f"🌬 Wind Speed : {data['wind']['speed']} m/s")
+
+            st.write(f"☁ Condition : {data['weather'][0]['description'].title()}")
+
+        else:
+            st.error("City not found.")
