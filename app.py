16ce27a9f222f1f3afd050c73ccce81c525eaee2
shakeel5552005 <shakeel5552005@gmail.com>
2026-07-11 22:37:13 +0500

    a
    a

--- app.py
+++ app.py
@@ -1,113 +1,13 @@
 import streamlit as st
-import pandas as pd
-
-# -----------------------
-# Page Config
-# -----------------------
 
 st.set_page_config(
-    page_title="Smart Excel Analyzer",
+    page_title="AI Data Analysis Assistant",
     page_icon="📊",
     layout="wide"
 )
 
-# -----------------------
-# Load CSS
-# -----------------------
+st.title("📊 AI Data Analysis Assistant")
 
-#with open("assets/style.css") as f:
-#    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+st.success("Project setup completed successfully!")
 
-# -----------------------
-# Sidebar
-# -----------------------
-
-st.sidebar.title("📂 Navigation")
-
-menu = st.sidebar.radio(
-    "Choose Option",
-    [
-        "Dashboard",
-        "Upload Excel",
-        "About"
-    ]
-)
-
-# -----------------------
-# Header
-# -----------------------
-
-st.markdown(
-    "<p class='main-title'>📊 Smart Excel Analyzer Pro</p>",
-    unsafe_allow_html=True
-)
-
-st.markdown(
-    "<p class='sub-title'>Analyze Excel files with interactive dashboard.</p>",
-    unsafe_allow_html=True
-)
-
-st.divider()
-
-# -----------------------
-# Dashboard
-# -----------------------
-
-if menu == "Dashboard":
-
-    col1, col2, col3 = st.columns(3)
-
-    col1.metric("📄 Files", 0)
-
-    col2.metric("📊 Rows", 0)
-
-    col3.metric("📈 Columns", 0)
-
-    st.info("Upload an Excel file from the sidebar.")
-
-# -----------------------
-# Upload
-# -----------------------
-
-elif menu == "Upload Excel":
-
-    uploaded_file = st.file_uploader(
-        "Upload Excel File",
-        type=["xlsx", "xls", "csv"]
-    )
-
-    if uploaded_file:
-
-        if uploaded_file.name.endswith(".csv"):
-
-            df = pd.read_csv(uploaded_file)
-
-        else:
-
-            df = pd.read_excel(uploaded_file)
-
-        st.success("File uploaded successfully!")
-
-        st.dataframe(df, use_container_width=True)
-
-# -----------------------
-# About
-# -----------------------
-
-else:
-
-    st.header("About")
-
-    st.write("""
-    Smart Excel Analyzer Pro
-
-    Developed using:
-
-    ✔ Streamlit
-
-    ✔ Pandas
-
-    ✔ Plotly
-
-    ✔ Python1
-    """)
+st.write("Welcome! In the next lesson, we'll upload and analyze CSV files.")
