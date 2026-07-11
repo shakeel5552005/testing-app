86a6c9a62c967bcfe6d80f3545d9a1e2ff3caa46
shakeel5552005 <shakeel5552005@gmail.com>
2026-07-12 01:16:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,4 @@
 import streamlit as st
-import pandas as pd
-from charts import create_chart
-from qa import answer_question
 
-# ----------------------------------
-# Page Configuration
-# ----------------------------------
-st.set_page_config(
-    page_title="AI Data Analysis Assistant",
-    page_icon="📊",
-    layout="wide"
-)
-
-st.title("📊 AI Data Analysis Assistant")
-st.write("Upload a CSV file to analyze your data.")
-
+st.title("Hello World")
+st.success("App is working")
